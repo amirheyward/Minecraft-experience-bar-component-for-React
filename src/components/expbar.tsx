98f3b19901c5b2af9d13ef6052add377aaac3b47
Amir Heyward <amirheyward2@gmail.com>
2026-06-07 16:25:38 -0400

    added timer

--- src/components/expbar.tsx
+++ src/components/expbar.tsx
@@ -1,7 +1,7 @@
 import "./expbar.css";
 import emptyBar from "../assets/xp_bar_empty.png";
 import fullBar from "../assets/xp_bar_full.png";
-import { useState } from "react";
+import { useState, useEffect, useRef} from "react";
 import gainExp from '../assets/gainxp.opus'
 import levelUp from '../assets/level up.opus'
 
@@ -13,22 +13,41 @@ interface EXPProps {
 function EXPBar(props: EXPProps) {
   const [level, setLevel] = useState(props.level ?? 0);
   const [percent, setPercent] = useState(props.percent ?? 0);
+  const prevPercentRef = useRef(percent);
 
   const gainExpAudio = new Audio(gainExp);
   const levelUpAudio = new Audio(levelUp);
 
-  function addBar() {
-    setPercent(percent + 0.1);
+  // sound playing and state updating for levels
+  useEffect(() => {
+    const prev = prevPercentRef.current;
 
+    if (percent < prev) {
+        setLevel(l => l + 1);
+        levelUpAudio.play();
+    } else if (percent > prev) { // will always run when this effect is called since it relies on percent
+        gainExpAudio.play();
+    }
+
+    prevPercentRef.current = percent;
+  }, [percent])
+
+  // timer for experience gain
+  useEffect(() => {
+    const id = setInterval(() => {
+        addBar();
+    }, 60000); // once a minute
+
+    return () => clearInterval(id);
+  }, [])
+
+  function addBar() {
+    // state updater functions must have no side effects
     setPercent(prev => {
       const next = prev + 0.1;
       if (next >= 1) {
-        setLevel(level + 1);
-        levelUpAudio.play();
         return next - 1;
       }
-
-      gainExpAudio.play();
       return next;
     });
   }
